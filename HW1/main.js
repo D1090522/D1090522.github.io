980d83399bb0e4e0c3d9da722b715b09376c228c
D1090522 <d1090522@o365.fcu.edu.tw>
2023-03-19 22:36:48 +0800

    first commit

---
+++ HW1/main.js
@@ -0,0 +1,16 @@
+var seconds = 30;
+
+// 取得倒數計時元素
+var countdownElement = document.getElementById("countdown");
+
+// 開始倒數計時
+var countdownInterval = setInterval(function() {
+    seconds--;
+    countdownElement.innerText = seconds;
+
+    // 如果倒數時間結束，則重置頁面
+    if (seconds == 0) {
+        clearInterval(countdownInterval);
+        location.reload();
+    }
+}, 1000);
